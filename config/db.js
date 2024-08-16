8a29843f4d6d37bb840492b368561e3ce1cdfd43
Zain Ali <zleo3883@gmail.com>
2024-08-16 15:55:25 +0500

    add mongo atlas link

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
     await mongoose
-      .connect(process.env.dburl)
+      .connect(
+        "mongodb+srv://zain114567:50I3VPy4fzcsA9t9@myprojects.ztxj7e9.mongodb.net/Welcomepass?retryWrites=true&w=majority"
+      )
       .then(() => {
         console.log("db is conncted");
       })
